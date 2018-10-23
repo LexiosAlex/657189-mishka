54c3a475094eeb59e1420dc2bd4aba9e77f2d131
LexiosAlex <lexiosma@gmail.com>
2018-10-23 07:37:06 +0300

    Сделал js

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,42 @@
+var menuMobile = document.querySelector(".main-nav__wrapper");
+var menuToggle = document.querySelector(".main-nav__toggle");
+var mobileScript = window.matchMedia("all and (max-width: 667px)");
+var orderWeeklyGood = document.querySelector(".weekly-goods__btn");
+var CartBtn = document.querySelector(".good__cart");
+var popup = document.querySelector(".overlay");
+
+  orderWeeklyGood.addEventListener("click", function (evt){
+      evt.preventDefault();
+      popup.classList.add("overlay-show");
+  });
+
+  CartBtn.addEventListener("click", function (evt){
+      evt.preventDefault();
+      popup.classList.add("overlay-show");
+  });
+
+
+  popup.addEventListener("click", function (evt) {
+      evt.preventDefault();
+      popup.classList.remove("overlay-show");
+    });
+
+
+  if (mobileScript.matches) {
+    menuMobile.classList.add("main-nav__wrapper--hidden");
+    menuToggle.classList.add("main-nav__toggle--on");
+
+    menuToggle.addEventListener("click", function (evt){
+      evt.preventDefault();
+      menuMobile.classList.remove("main-nav__wrapper--hidden");
+      menuToggle.classList.remove("main-nav__toggle--on");
+      menuToggle.classList.add("main-nav__toggle--off");
+    });
+
+  } else {
+
+  }
+
 // Функция ymaps.ready() будет вызвана, когда
 // загрузятся все компоненты API, а также когда будет готово DOM-дерево.
     ymaps.ready(function () {
@@ -14,14 +53,14 @@
     });
 
     var myPlacemark = new ymaps.Placemark([59.938631, 30.323055], {
-        hintContent: 'Санкт-Петербург, Ул.Большая Конюшенная 19/8, офис 101',
+        hintContent: "Санкт-Петербург, Ул.Большая Конюшенная 19/8, офис 101",
     },
     {
         // Опции.
         // Необходимо указать данный тип макета.
-        iconLayout: 'default#image',
+        iconLayout: "default#image",
         // Своё изображение иконки метки.
-        iconImageHref: '../img/icon-map-pin.svg',
+        iconImageHref: "../img/icon-map-pin.svg",
         // Размеры метки.
         iconImageSize: [85, 140],
         // Смещение левого верхнего угла иконки относительно
@@ -30,3 +69,4 @@
     });
     myMap.geoObjects.add(myPlacemark);
 })
+
