1620d9a2438556646394e1181a81b828d96eee32
LexiosAlex <lexiosma@gmail.com>
2018-10-26 03:28:53 +0300

    Сделал JS

--- source/js/script.js
+++ source/js/script.js
@@ -1,21 +1,6 @@
 var menuMobile = document.querySelector(".main-nav__wrapper");
 var menuToggle = document.querySelector(".main-nav__toggle");
 var mobileScript = window.matchMedia("all and (max-width: 667px)");
-// var pupupCheck = document.getElemntByClassName(".overlay");
-var popup = document.querySelector(".overlay");
-var popupShow = document.querySelector(".popup-show");
-
-if (popup.classList.contains('.overlay')) {
-  popupShow.addEventListener("click", function (evt){
-      evt.preventDefault();
-      popup.classList.add("overlay-show");
-  });
-
-  popup.onclick = function(evt) {
-      evt.preventDefault();
-      popup.classList.remove("overlay-show");
-    };
- }
 
   if (mobileScript.matches) {
     menuMobile.classList.add("main-nav__wrapper--hidden");
@@ -40,37 +25,54 @@ if (popup.classList.contains('.overlay')) {
 
   };
 
-// Функция ymaps.ready() будет вызвана, когда
-// загрузятся все компоненты API, а также когда будет готово DOM-дерево.
-    ymaps.ready(function () {
-    // Создание карты.
-    var myMap = new ymaps.Map("map", {
-        // Координаты центра карты.
-        // Порядок по умолчнию: «широта, долгота».
-        // Чтобы не определять координаты центра карты вручную,
-        // воспользуйтесь инструментом Определение координат.
-        center: [59.939931, 30.323],
-        // Уровень масштабирования. Допустимые значения:
-        // от 0 (весь мир) до 19.
-        zoom: 16
-    });
+var popup = document.querySelector(".overlay");
+var modal = document.querySelector(".modal-add-item");
+var WeeklyGood = document.querySelector(".weekly-goods__btn");
+var CatalogBtns = document.querySelectorAll(".popup-show");
 
-    var myPlacemark = new ymaps.Placemark([59.938631, 30.323055], {
-        hintContent: "Санкт-Петербург, Ул.Большая Конюшенная 19/8, офис 101",
-    },
-    {
-        // Опции.
-        // Необходимо указать данный тип макета.
-        iconLayout: "default#image",
-        // Своё изображение иконки метки.
-        iconImageHref: "../img/icon-map-pin.svg",
-        // Размеры метки.
-        iconImageSize: [85, 140],
-        // Смещение левого верхнего угла иконки относительно
-        // её "ножки" (точки привязки).
-        iconImageOffset: [-50, -140]
+  if (WeeklyGood) {
+    WeeklyGood.addEventListener("click", function(event) {
+      event.preventDefault();
+      modal.classList.add("modal-show");
+      popup.classList.add("overlay-show");
     });
-    myMap.geoObjects.add(myPlacemark);
-});
+  }
 
+  if (CatalogBtns.length !== 0) {
+    for (var i = 0; i < CatalogBtns.length; i++) {
+      CatalogBtns[i].addEventListener("click", function(event) {
+        event.preventDefault();
+        modal.classList.add("modal-show");
+        popup.classList.add("overlay-show");
+      });
+    }
+  }
+
+  if (popup) {
+    popup.addEventListener("click", function () {
+      if (popup.classList.contains("overlay-show")) {
+        modal.classList.remove("modal-show");
+        popup.classList.remove("overlay-show");
+      }
+    });
+  }
+
+var map;
+  function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 17,
+      center: new google.maps.LatLng(59.938508, 30.323126),
+      mapTypeId: 'roadmap'
+    });
+    var marker = new google.maps.Marker({
+
+    position: new google.maps.LatLng(59.938631, 30.323055),
+    map: map,
+    draggable:true,
+    icon: "../img/icon-map-pin.svg",
+    size: new google.maps.Size(85, 140),
+    scaledSize: new google.maps.Size(85, 140)
+
+    });
+  }
 
